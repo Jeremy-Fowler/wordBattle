4f737b0151713fa6197a4693666dad8120695b4a
Jeremy Fowler <jembyfowler@gmail.com>
2022-12-11 14:24:57 -0700

    init

---
+++ wordBattle.client/src/env.js
@@ -0,0 +1,6 @@
+export const dev = window.location.origin.includes('localhost')
+export const baseURL = dev ? 'http://localhost:3000' : ''
+export const useSockets = false
+export const domain = 'jeremy-is-cool.us.auth0.com'
+export const clientId = 'i8i46RsQKfs43bbUpohFpr1bw7M5xYak'
+export const audience = 'https://student-auth.com'
